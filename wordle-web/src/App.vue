0c5b1668056c6ba731af211c0d8f1f324de8ed1c
czamora7 <czamora4@ewu.edu>
2023-04-27 21:47:38 -0700

    started part 2

--- wordle-web/src/App.vue
+++ wordle-web/src/App.vue
@@ -1,13 +1,33 @@
 <template>
   <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink> | <RouterLink to="/wordle">Wordle</RouterLink> |
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-      <v-btn @click="setInverseTheme"> Inverse Theme </v-btn>
-      <v-btn @click="setDarkTheme"> Dark Theme </v-btn>
-    </div>
+      <div class="wrapper">
+          <v-app-bar :elevation="2">
+              <v-app-bar-title>Wordle 2: Electric Boogaloo</v-app-bar-title>
+              <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+          </v-app-bar>
+
+          <v-navigation-drawer v-model="drawer" absolute bottom temporary>
+              <v-list nav dense>
+                  <v-list-item-group v-model="group">
+                      <v-list-item>
+                          <nav>
+                              <RouterLink to="/wordle">Wordle</RouterLink>
+                          </nav>
+                      </v-list-item>
+
+                      <v-list-item>
+                          <nav>
+                             <RouterLink to="/about">About</RouterLink>
+                          </nav>
+                      </v-list-item>
+
+                  </v-list-item-group>
+              </v-list>
+          </v-navigation-drawer>
+
+          <v-btn @click="setInverseTheme"> Inverse Theme </v-btn>
+          <v-btn @click="setDarkTheme"> Dark Theme </v-btn>
+      </div>
   </header>
 
   <RouterView />
