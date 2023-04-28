1470bc60cc72b0bce3046adec2a88dfb383451e9
czamora7 <czamora4@ewu.edu>
2023-04-27 21:50:50 -0700

    ran prettier

--- wordle-web/src/App.vue
+++ wordle-web/src/App.vue
@@ -1,33 +1,32 @@
 <template>
   <header>
-      <div class="wrapper">
-          <v-app-bar :elevation="2">
-              <v-app-bar-title>Wordle 2: Electric Boogaloo</v-app-bar-title>
-              <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-          </v-app-bar>
+    <div class="wrapper">
+      <v-app-bar :elevation="2">
+        <v-app-bar-title>Wordle 2: Electric Boogaloo</v-app-bar-title>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      </v-app-bar>
 
-          <v-navigation-drawer v-model="drawer" absolute bottom temporary>
-              <v-list nav dense>
-                  <v-list-item-group v-model="group">
-                      <v-list-item>
-                          <nav>
-                              <RouterLink to="/wordle">Wordle</RouterLink>
-                          </nav>
-                      </v-list-item>
+      <v-navigation-drawer v-model="drawer" absolute bottom temporary>
+        <v-list nav dense>
+          <v-list-item-group v-model="group">
+            <v-list-item>
+              <nav>
+                <RouterLink to="/wordle">Wordle</RouterLink>
+              </nav>
+            </v-list-item>
 
-                      <v-list-item>
-                          <nav>
-                             <RouterLink to="/about">About</RouterLink>
-                          </nav>
-                      </v-list-item>
+            <v-list-item>
+              <nav>
+                <RouterLink to="/about">About</RouterLink>
+              </nav>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
 
-                  </v-list-item-group>
-              </v-list>
-          </v-navigation-drawer>
-
-          <v-btn @click="setInverseTheme"> Inverse Theme </v-btn>
-          <v-btn @click="setDarkTheme"> Dark Theme </v-btn>
-      </div>
+      <v-btn @click="setInverseTheme"> Inverse Theme </v-btn>
+      <v-btn @click="setDarkTheme"> Dark Theme </v-btn>
+    </div>
   </header>
 
   <RouterView />
